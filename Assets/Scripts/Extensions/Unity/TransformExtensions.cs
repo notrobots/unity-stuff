c9918cc6d03a7a470ce2f398534cfcf0ef626c85
maxpilotto <maxpil99@outlook.com>
2023-06-29 20:59:40 +0200

    Add SetEulerX, SetEulerY and SetEulerZ

---
+++ Assets/Scripts/Extensions/Unity/TransformExtensions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public static class TransformExtensions
+{
+    public static void SetEulerX(this Transform transform, float value)
+    {
+        var current = transform.rotation.eulerAngles;
+
+        transform.rotation = Quaternion.Euler(value, current.y, current.z);
+    }
+
+    public static void SetEulerY(this Transform transform, float value)
+    {
+        var current = transform.rotation.eulerAngles;
+
+        transform.rotation = Quaternion.Euler(current.x, value, current.z);
+    }
+
+    public static void SetEulerZ(this Transform transform, float value)
+    {
+        var current = transform.rotation.eulerAngles;
+
+        transform.rotation = Quaternion.Euler(current.x, current.y, value);
+    }
+}
