fbab533659a67f12c28d81b6ac44d623c44b899d
maxpilotto <maxpil99@outlook.com>
2023-06-29 21:00:05 +0200

    Add SimplePropertyDrawer

---
+++ Assets/Editor/Scripts/PropertyDrawers/SimplePropertyDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(SimplePropertyAttribute))]
+public class SimplePropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var targetObject = property.serializedObject.targetObject;
+        var targetObjectClassType = targetObject.GetType();
+        var field = targetObjectClassType.GetField(property.propertyPath);
+
+        if (field != null)
+        {
+            var value = field.GetValue(targetObject);
+            EditorGUI.LabelField(position, ObjectNames.NicifyVariableName(property.name), value.ToString());
+        }
+    }
+}
