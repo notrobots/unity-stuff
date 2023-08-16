f34df48544ba177f7e9b4d33ee73b26697bbc84a
maxpilotto <maxpil99@outlook.com>
2023-08-16 11:35:59 +0200

    Add MathUtil

---
+++ Assets/Scripts/Utils/MathUtil.cs
@@ -0,0 +1,16 @@
+﻿public static class MathUtil
+{
+    public static double Average(int startInclusive, int endInclusive)
+    {
+        double sum = 0;
+        double count = 0;
+
+        for (int i = startInclusive; i < endInclusive + 1; i++)
+        {
+            sum += i;
+            count++;
+        }
+
+        return sum / count;
+    }
+}
