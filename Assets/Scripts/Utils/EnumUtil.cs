9b571a63da05b959e81b53391a55342c8cf0b643
maxpilotto <maxpil99@outlook.com>
2023-09-04 15:59:33 +0200

    Create EnumUtil.cs

---
+++ Assets/Scripts/Utils/EnumUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class EnumUtil
+{
+    public static IEnumerable<E> ExtractFlags<E>(E value)
+        where E : Enum
+    {
+        foreach (E enumValue in Enum.GetValues(typeof(E)))
+        {
+            if (value.HasFlag(enumValue))
+            {
+                yield return enumValue;
+            }
+        }
+    }
+
+    public static void AddFlag<E>(ref E flags, E flag)
+        where E : Enum
+    {
+        flags = (E)(object)((int)(object)flags | (int)(object)flag);
+    }
+
+    public static void RemoveFlag<E>(ref E flags, E flag)
+        where E : Enum
+    {
+        flags = (E)(object)((int)(object)flags & (~(int)(object)flag));
+    }
+}
